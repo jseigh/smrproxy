30f779af7dc9556d121a71d523280f3f4dfb8713
user0 <user0@localhost.localdomain>
2023-09-10 13:55:41 -0400

    clean up api and example2 test

--- test/example2.c
+++ test/example2.c
@@ -5,6 +5,8 @@
 #include <threads.h>
 #include <string.h>
 
+#include <pthread.h>
+
 #include <smrproxy.h>
 
 /**
@@ -28,18 +30,26 @@ typedef struct {
     mtx_t mutex;
     cnd_t cvar;
 
+    pthread_barrier_t barrier;
+
 } queue_t;
 
-static epoch_t getexpiry(void *x)
+static epoch_t getexpiry(void *x, void *ctx)
 {
     pnode_t node = (pnode_t) x;
     return node->expiry;
 }
 
+static void setexpiry(epoch_t expiry, void *data, void *ctx)
+{
+    pnode_t node = (pnode_t) data;
+    atomic_store_explicit(&node->expiry, expiry, memory_order_relaxed);
+}
+
 static void free_node(void *x)
 {
     pnode_t node = (pnode_t) x;
-    fprintf(stdout, "free event_id=%d\n", node->event_id);
+    fprintf(stdout, "free event_id=%d expiry=%lu\n", node->event_id, node->expiry);
     free(x);
 }
 
@@ -61,17 +71,15 @@ static void push(queue_t *queue, const int event_id)
     mtx_lock(&queue->mutex);
 
     pnode_t current = queue->events;
-
-    epoch_t ref_epoch = smrproxy_get_epoch(queue->proxy);
-    atomic_store_explicit(&current->expiry, ref_epoch, memory_order_release);
-
     current->event_id = event_id;
+
     atomic_store_explicit(&current->next, node, memory_order_release);
-    // make node unreachable
     atomic_store_explicit(&queue->events, node, memory_order_release); 
 
-    fprintf(stdout, "retiring event_id=%d expiry=%lu\n", event_id, ref_epoch);
-    smrproxy_retire_async(queue->proxy, current, &free_node);
+    epoch_t expiry = smrproxy_get_epoch(queue->proxy);
+    fprintf(stdout, "retiring event_id=%d expiry=%lu\n", event_id, expiry);
+
+    smrproxy_retire_async_exp(queue->proxy, current, &free_node, &setexpiry, NULL);
 
     cnd_broadcast(&queue->cvar);
     mtx_unlock(&queue->mutex);
@@ -83,6 +91,10 @@ static void push(queue_t *queue, const int event_id)
 */
 static int listen(void *arg)
 {
+
+    time_t t = time(NULL);
+    srandom((int) t);
+
     queue_t *queue = (queue_t *) arg;
 
     thrd_t tid = thrd_current();
@@ -94,10 +106,13 @@ static int listen(void *arg)
 
     pnode_t node = atomic_load_explicit(&queue->events, memory_order_acquire);
 
+    pthread_barrier_wait(&queue->barrier);
+
     for (;;)
     {
 
-        if (node->next == NULL)
+        if (atomic_load_explicit(&node->next, memory_order_acquire) == NULL)
+        //if (node->next == NULL)
         {
             mtx_lock(&queue->mutex);
             while (node->next == NULL)
@@ -108,11 +123,12 @@ static int listen(void *arg)
         }
 
         epoch_t prev_epoch = ref->epoch;
-        smrproxy_ref_next(ref, &getexpiry, node);
+        smrproxy_ref_next(ref, &getexpiry, node, NULL);
         epoch_t next_epoch = ref->epoch;
         fprintf(stdout, "%lu) event_id=%d epoch: prev=%u next=%u\n", tid, node->event_id, prev_epoch, next_epoch);
 
-        sleep(500);
+        int adj = random() % 250;
+        sleep(300 + adj);
 
         if (node->event_id == -1)
             break;
@@ -139,6 +155,7 @@ int main(int argc, char **argv)
     queue->proxy = smrproxy_create(config);
     mtx_init(&queue->mutex, mtx_plain);
     cnd_init(&queue->cvar);
+    pthread_barrier_init(&queue->barrier, NULL, 3);
     queue->events = malloc(sizeof(node_t));
     memset(queue->events, 0, sizeof(node_t));
 
@@ -148,13 +165,12 @@ int main(int argc, char **argv)
     thrd_create(&reader, &listen, queue);
     thrd_create(&reader2, &listen, queue);
 
-    sleep(500);  // give reader threads chance to start
-
+    pthread_barrier_wait(&queue->barrier);
 
     for (int ndx = 1; ndx <= 20; ndx++)
     {
         push(queue, 1000 + ndx);
-        sleep(300);
+        sleep(200);
     }
     push(queue, -1);
 
@@ -163,6 +179,7 @@ int main(int argc, char **argv)
 
     smrproxy_destroy(queue->proxy);
     free_node(queue->events);
+    pthread_barrier_destroy(&queue->barrier);
     cnd_destroy(&queue->cvar);
     mtx_destroy(&queue->mutex);
     free(queue);
