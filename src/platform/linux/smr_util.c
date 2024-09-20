800ffb6e3438745b83d96ea3e2e7423d2665d36a
user0 <user0@localhost.localdomain>
2024-09-20 14:15:23 -0400

    update cmake

--- src/platform/linux/smr_util.c
+++ src/platform/linux/smr_util.c
@@ -1,5 +1,5 @@
 /*
-   Copyright 20203 Joseph W. Seigh
+   Copyright 2023 Joseph W. Seigh
    
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
